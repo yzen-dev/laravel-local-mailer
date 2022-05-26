0ea6cd9baa9bcd082d6416bd97cc234b26957bb8
Яценко Андрей <yzen.dev@gmail.ru>
2022-05-26 21:20:47 +0300

    Add: empty list

--- src/views/dashboard.blade.php
+++ src/views/dashboard.blade.php
@@ -1,54 +1,63 @@
 @extends('local-mailer::_template')
 
 @section('content')
-
-    <table class="table mt-4">
-        <thead class="table-light">
-        <tr>
-            <td>Logs</td>
-            <td class="text-center">Count</td>
-            <td></td>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($files as $file)
+    @if (count($files) > 0)
+        <table class="table mt-4">
+            <thead class="table-light">
             <tr>
-                <td>
-                    <a href="{{ route('local-mailer::show-by-date',$file['date']) }}">
-                        {{ $file['file'] }}
-                    </a>
-                </td>
-                <td class="text-center">
-                    <a href="{{ route('local-mailer::show-by-date',$file['date']) }}">
-                        {{ $file['count'] }}
-                    </a>
-                </td>
-                <td class="text-right">
-                    <a class="btn-success mr-2" href="{{ route('local-mailer::download-log',$file['date']) }}"
-                       target="_blank">
-                        <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" stroke-width="2"
-                             fill="none" stroke-linecap="round" stroke-linejoin="round"
-                             class="css-i6dzq1">
-                            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                            <polyline points="7 10 12 15 17 10"></polyline>
-                            <line x1="12" y1="15" x2="12" y2="3"></line>
-                        </svg>
-                    </a>
-                    <a class="btn-danger" href="{{ route('local-mailer::delete-log',$file['date']) }}">
-                        <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" stroke-width="2"
-                             fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <polyline points="3 6 5 6 21 6"></polyline>
-                            <path
-                                d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                            <line x1="10" y1="11" x2="10" y2="17"></line>
-                            <line x1="14" y1="11" x2="14" y2="17"></line>
-                        </svg>
-                    </a>
-                </td>
+                <td>Logs</td>
+                <td class="text-center">Count</td>
+                <td></td>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
-
+            </thead>
+            <tbody>
+            @foreach ($files as $file)
+                <tr>
+                    <td>
+                        <a href="{{ route('local-mailer::show-by-date',$file['date']) }}">
+                            {{ $file['file'] }}
+                        </a>
+                    </td>
+                    <td class="text-center">
+                        <a href="{{ route('local-mailer::show-by-date',$file['date']) }}">
+                            {{ $file['count'] }}
+                        </a>
+                    </td>
+                    <td class="text-right">
+                        <a class="btn-success mr-2" href="{{ route('local-mailer::download-log',$file['date']) }}"
+                           target="_blank">
+                            <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" stroke-width="2"
+                                 fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                 class="css-i6dzq1">
+                                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                <polyline points="7 10 12 15 17 10"></polyline>
+                                <line x1="12" y1="15" x2="12" y2="3"></line>
+                            </svg>
+                        </a>
+                        <a class="btn-danger" href="{{ route('local-mailer::delete-log',$file['date']) }}">
+                            <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" stroke-width="2"
+                                 fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <polyline points="3 6 5 6 21 6"></polyline>
+                                <path
+                                    d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                <line x1="10" y1="11" x2="10" y2="17"></line>
+                                <line x1="14" y1="11" x2="14" y2="17"></line>
+                            </svg>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="mailbox">
+            <div class="mailbox-head">
+                <h2 class="mailbox-head_title">Mail logs</h2>
+            </div>
+            <div class="box-center">
+                <h2>List empty</h2>
+            </div>
+        </div>
+    @endif
 
 @endsection
--- src/views/mails.blade.php
+++ src/views/mails.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <a href="{{ route('local-mailer::dashboard') }}" class="back-link">
-        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none"
+             stroke-linecap="round" stroke-linejoin="round">
             <line x1="19" y1="12" x2="5" y2="12"></line>
             <polyline points="12 19 5 12 12 5"></polyline>
         </svg>
@@ -13,30 +14,36 @@
             <div class="mailbox-head">
                 <h2 class="mailbox-head_title">Mail for {{$date}}</h2>
             </div>
-            <div class="mailbox-body">
-                <div class="mail-list">
-                    @foreach ($mails as $mail)
-                        <div class="mail-preview" data-mail-frame="{{$mail->body}}"
-                             data-mail-recipient="{{json_encode($mail->to)}}">
-                            <div class="mail-preview_subject">
-                                {{$mail->subject}}
+            @if (count($mails) > 0)
+                <div class="mailbox-body">
+                    <div class="mail-list">
+                        @foreach ($mails as $mail)
+                            <div class="mail-preview" data-mail-frame="{{$mail->body}}"
+                                 data-mail-recipient="{{json_encode($mail->to)}}">
+                                <div class="mail-preview_subject">
+                                    {{$mail->subject}}
+                                </div>
+                                <div class="mail-preview_date">
+                                    {{date_format(date_create($mail->date), 'H:i:s')}}
+                                </div>
                             </div>
-                            <div class="mail-preview_date">
-                                {{date_format(date_create($mail->date), 'H:i:s')}}
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-                <div class="mail-content">
-                    <div class="mail-recipient">
-                        Recipient
-                        <div class="mail-recipient_list" id="mailRecipient">
-                        </div>
+                        @endforeach
                     </div>
-                    <iframe class="mail-frame" id="mailFrame">
-                    </iframe>
+                    <div class="mail-content">
+                        <div class="mail-recipient">
+                            Recipient
+                            <div class="mail-recipient_list" id="mailRecipient">
+                            </div>
+                        </div>
+                        <iframe class="mail-frame" id="mailFrame">
+                        </iframe>
+                    </div>
                 </div>
-            </div>
+            @else
+                <div class="box-center">
+                    <h2>List empty</h2>
+                </div>
+            @endif
         </div>
     </div>
     <script>
